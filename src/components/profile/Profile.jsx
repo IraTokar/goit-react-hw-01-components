961a23056a399ad0e571c412d6acbe419c9f55e1
IraTokar <tokar.ira20@gmail.com>
2024-05-30 18:41:23 +0200

    search error

--- src/components/profile/Profile.jsx
+++ src/components/profile/Profile.jsx
@@ -3,32 +3,31 @@ import {ProfileWrap, Description, UserAvatar, Name, Tag, Location, StatsWrap, St
 
 const Profile = ({username,tag,location,avatar,stats}) => {
   return (
-<ProfileWrap>
-  <Description>
-    <UserAvatar
-      src={avatar}
-      alt={username}
-    />
-    <Name>{username}</Name>
-    <Tag>@{tag}</Tag>
-    <Location>{location}</Location>
-  </Description>
+    <ProfileWrap>
+      <Description>
+        <UserAvatar
+          src={avatar}
+          alt={username} />
+        <Name>{username}</Name>
+        <Tag>@{tag}</Tag>
+        <Location>{location}</Location>
+      </Description>
 
-  <StatsWrap>
-    <StatsItem>
-      <Label>Followers</Label>
-      <Quantity>{stats.followers}</Quantity>
-    </StatsItem>
-    <StatsItem>
-      <Label>Views</Label>
-      <Quantity>{stats.views}</Quantity>
-    </StatsItem>
-    <StatsItem>
-      <Label>Likes</Label>
-      <Quantity>{stats.likes}</Quantity>
-    </StatsItem>
-  </StatsWrap>
-</ProfileWrap>
+      <StatsWrap>
+        <StatsItem>
+          <Label>Followers</Label>
+          <Quantity>{stats.followers}</Quantity>
+        </StatsItem>
+        <StatsItem>
+          <Label>Views</Label>
+          <Quantity>{stats.views}</Quantity>
+        </StatsItem>
+        <StatsItem>
+          <Label>Likes</Label>
+          <Quantity>{stats.likes}</Quantity>
+        </StatsItem>
+      </StatsWrap>
+    </ProfileWrap>
   );
 };
 
