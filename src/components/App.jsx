00ccd3bee22c2aa63571a45fdeb9dcea6ad7719b
IraTokar <tokar.ira20@gmail.com>
2024-05-30 12:58:18 +0200

    tasks complete

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -16,7 +16,11 @@ import TransactionHistory from './TransactionHistory/TransactionHistory';
 export default function App() {
   return (
     <>
-    <Profile user={user} />
+    <Profile username={user.username}
+        tag={user.tag}
+        location={user.location}
+        avatar={user.avatar}
+        stats={user.stats} />
     <Statistics title="Upload stats" stats={data} />
     <FriendList friends={friends} />
     <TransactionHistory items={items} />
